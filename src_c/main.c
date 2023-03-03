5287bf2931cb5aff448e29618ab121e0b08056a3
salticecream <shortgolind@gmail.com>
2023-03-03 14:03:36 +0100

    add c insertion sort

--- src_c/main.c
+++ src_c/main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
-void print_list(int *list, int len) {
+void print_list(const int* arr, const int len) {
     for(int i = 0; i < len; i++) {
-        printf("%d ", list[i]);
+        printf("%d ", arr[i]);
     }
     printf("\n");
+}
+
+
+// a and b are not the indices, they are the addresses of the values
+void swap(int* a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void isort(int* arr, const int len) {
+    for(int i = 1; i < len; i++) {
+        int j = i;
+        while(j > 0 && arr[j-1] > arr[j]) {
+            swap(&arr[j], &arr[j-1]);
+            j -= 1;
+        }
+    }
 }
 
 int main() {
@@ -18,6 +36,8 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
+print_list(arr, len);
+isort(arr, len);
 print_list(arr, len);
 }
 
